5ced161de585601fc19b04f4ad4fa087683c364c
unknown <nam030920@gmail.com>
2021-11-15 15:14:53 +0700

    Commit

--- GUI_PresentationLayer/Form_DSTour.Designer.cs
+++ GUI_PresentationLayer/Form_DSTour.Designer.cs
@@ -61,13 +61,12 @@ namespace GUI_PresentationLayer
             this.col_4,
             this.col_5});
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
-            this.dataGridView1.Location = new System.Drawing.Point(45, 391);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(36, 313);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 50;
-            this.dataGridView1.Size = new System.Drawing.Size(1095, 465);
+            this.dataGridView1.Size = new System.Drawing.Size(876, 372);
             this.dataGridView1.TabIndex = 4;
             this.dataGridView1.VirtualMode = true;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
@@ -105,28 +104,28 @@ namespace GUI_PresentationLayer
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(424, 45);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(339, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(288, 45);
+            this.label1.Size = new System.Drawing.Size(239, 37);
             this.label1.TabIndex = 5;
             this.label1.Text = "DANH SÁCH TOUR";
             // 
             // tb_TiemKiem
             // 
-            this.tb_TiemKiem.Location = new System.Drawing.Point(643, 259);
+            this.tb_TiemKiem.Location = new System.Drawing.Point(643, 257);
             this.tb_TiemKiem.Margin = new System.Windows.Forms.Padding(2);
             this.tb_TiemKiem.Multiline = true;
             this.tb_TiemKiem.Name = "tb_TiemKiem";
-            this.tb_TiemKiem.Size = new System.Drawing.Size(269, 30);
+            this.tb_TiemKiem.Size = new System.Drawing.Size(269, 33);
             this.tb_TiemKiem.TabIndex = 18;
             this.tb_TiemKiem.TextChanged += new System.EventHandler(this.tb_TiemKiem_TextChanged);
             // 
             // btn_Them
             // 
-            this.btn_Them.Location = new System.Drawing.Point(36, 259);
+            this.btn_Them.Location = new System.Drawing.Point(36, 257);
+            this.btn_Them.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Them.Name = "btn_Them";
-            this.btn_Them.Size = new System.Drawing.Size(130, 30);
+            this.btn_Them.Size = new System.Drawing.Size(104, 33);
             this.btn_Them.TabIndex = 20;
             this.btn_Them.Text = "Thêm";
             this.btn_Them.UseVisualStyleBackColor = true;
@@ -135,100 +134,90 @@ namespace GUI_PresentationLayer
             // lb_TenTour
             // 
             this.lb_TenTour.AutoSize = true;
-            this.lb_TenTour.Location = new System.Drawing.Point(45, 114);
-            this.lb_TenTour.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_TenTour.Location = new System.Drawing.Point(36, 91);
             this.lb_TenTour.Name = "lb_TenTour";
-            this.lb_TenTour.Size = new System.Drawing.Size(86, 25);
+            this.lb_TenTour.Size = new System.Drawing.Size(72, 20);
             this.lb_TenTour.TabIndex = 21;
             this.lb_TenTour.Text = "Tên Tour :";
             // 
             // tb__TenTour
             // 
-            this.tb__TenTour.Location = new System.Drawing.Point(45, 160);
-            this.tb__TenTour.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb__TenTour.Location = new System.Drawing.Point(36, 128);
             this.tb__TenTour.Name = "tb__TenTour";
-            this.tb__TenTour.Size = new System.Drawing.Size(299, 31);
+            this.tb__TenTour.Size = new System.Drawing.Size(240, 27);
             this.tb__TenTour.TabIndex = 22;
             // 
             // lb_TenKhachSan
             // 
             this.lb_TenKhachSan.AutoSize = true;
-            this.lb_TenKhachSan.Location = new System.Drawing.Point(45, 198);
-            this.lb_TenKhachSan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_TenKhachSan.Location = new System.Drawing.Point(36, 158);
             this.lb_TenKhachSan.Name = "lb_TenKhachSan";
-            this.lb_TenKhachSan.Size = new System.Drawing.Size(133, 25);
+            this.lb_TenKhachSan.Size = new System.Drawing.Size(111, 20);
             this.lb_TenKhachSan.TabIndex = 23;
             this.lb_TenKhachSan.Text = "Tên Khách Sạn :";
             // 
             // tb_TenKhachSan
             // 
-            this.tb_TenKhachSan.Location = new System.Drawing.Point(45, 240);
-            this.tb_TenKhachSan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_TenKhachSan.Location = new System.Drawing.Point(36, 192);
             this.tb_TenKhachSan.Name = "tb_TenKhachSan";
-            this.tb_TenKhachSan.Size = new System.Drawing.Size(299, 31);
+            this.tb_TenKhachSan.Size = new System.Drawing.Size(240, 27);
             this.tb_TenKhachSan.TabIndex = 24;
             // 
             // tb_DacDiem
             // 
-            this.tb_DacDiem.Location = new System.Drawing.Point(431, 160);
-            this.tb_DacDiem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_DacDiem.Location = new System.Drawing.Point(345, 128);
             this.tb_DacDiem.Name = "tb_DacDiem";
-            this.tb_DacDiem.Size = new System.Drawing.Size(290, 31);
+            this.tb_DacDiem.Size = new System.Drawing.Size(233, 27);
             this.tb_DacDiem.TabIndex = 25;
             // 
             // tb_NoiDung
             // 
-            this.tb_NoiDung.Location = new System.Drawing.Point(804, 160);
-            this.tb_NoiDung.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_NoiDung.Location = new System.Drawing.Point(643, 128);
             this.tb_NoiDung.Multiline = true;
             this.tb_NoiDung.Name = "tb_NoiDung";
-            this.tb_NoiDung.Size = new System.Drawing.Size(335, 113);
+            this.tb_NoiDung.Size = new System.Drawing.Size(269, 91);
             this.tb_NoiDung.TabIndex = 26;
             // 
             // lb_DacDiem
             // 
             this.lb_DacDiem.AutoSize = true;
-            this.lb_DacDiem.Location = new System.Drawing.Point(431, 119);
-            this.lb_DacDiem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_DacDiem.Location = new System.Drawing.Point(345, 95);
             this.lb_DacDiem.Name = "lb_DacDiem";
-            this.lb_DacDiem.Size = new System.Drawing.Size(98, 25);
+            this.lb_DacDiem.Size = new System.Drawing.Size(82, 20);
             this.lb_DacDiem.TabIndex = 27;
             this.lb_DacDiem.Text = "Đặc Điểm :";
             // 
             // lb_LoaiHinh
             // 
             this.lb_LoaiHinh.AutoSize = true;
-            this.lb_LoaiHinh.Location = new System.Drawing.Point(435, 198);
-            this.lb_LoaiHinh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_LoaiHinh.Location = new System.Drawing.Point(348, 158);
             this.lb_LoaiHinh.Name = "lb_LoaiHinh";
-            this.lb_LoaiHinh.Size = new System.Drawing.Size(95, 25);
+            this.lb_LoaiHinh.Size = new System.Drawing.Size(79, 20);
             this.lb_LoaiHinh.TabIndex = 28;
             this.lb_LoaiHinh.Text = "Loại Hình :";
             // 
             // lb_NoiDung
             // 
             this.lb_NoiDung.AutoSize = true;
-            this.lb_NoiDung.Location = new System.Drawing.Point(804, 119);
-            this.lb_NoiDung.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_NoiDung.Location = new System.Drawing.Point(643, 95);
             this.lb_NoiDung.Name = "lb_NoiDung";
-            this.lb_NoiDung.Size = new System.Drawing.Size(98, 25);
+            this.lb_NoiDung.Size = new System.Drawing.Size(80, 20);
             this.lb_NoiDung.TabIndex = 29;
             this.lb_NoiDung.Text = "Nội Dung :";
             // 
             // cb_LoaiHinh
             // 
             this.cb_LoaiHinh.FormattingEnabled = true;
-            this.cb_LoaiHinh.Location = new System.Drawing.Point(435, 239);
-            this.cb_LoaiHinh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cb_LoaiHinh.Location = new System.Drawing.Point(348, 191);
             this.cb_LoaiHinh.Name = "cb_LoaiHinh";
-            this.cb_LoaiHinh.Size = new System.Drawing.Size(290, 33);
+            this.cb_LoaiHinh.Size = new System.Drawing.Size(233, 28);
             this.cb_LoaiHinh.TabIndex = 30;
             // 
             // Form_DSTour
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1201, 871);
+            this.ClientSize = new System.Drawing.Size(961, 697);
             this.Controls.Add(this.cb_LoaiHinh);
             this.Controls.Add(this.lb_NoiDung);
             this.Controls.Add(this.lb_LoaiHinh);
@@ -244,7 +233,6 @@ namespace GUI_PresentationLayer
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form_DSTour";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý Tour";
