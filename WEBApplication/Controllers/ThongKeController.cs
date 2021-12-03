daecf8eacfbe31bb1f9fb5f67f1fd0b1ac8ba202
KNhaa <kimnha2121@gmail.com>
2021-12-03 16:58:40 +0700

    Design ThongKe Button

---
+++ WEBApplication/Controllers/ThongKeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBApplication.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        // tk chi phí
+        public IActionResult TK_ChiPhi()
+        {
+            return View();
+           
+        }
+        public IActionResult TK_DoanhThu()
+        {
+            return View();
+
+        }
+        public IActionResult TK_NhanVien()
+        {
+            return View();
+
+        }
+        public IActionResult TK_Tour()
+        {
+            return View();
+
+        }
+
+    }
+}
